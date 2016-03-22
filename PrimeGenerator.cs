b219e9b27b68ae58d2fb5eb46f964ef8bcefead8
Mblikolo <rusakov-rv@yandex.ru>
2016-03-22 18:46:55 +0500

    Наивная реализация решета Эратосфена

---
+++ PrimeGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround
+{
+    class PrimeGenerator
+    {
+        public static int[] Generate(int maxValue)
+        {
+            if (maxValue < 2)
+                return new int[0];
+
+            var f = new bool[maxValue + 1];
+            f[0] = f[1] = true;
+            //0 - значит простое
+
+            for (int i = 2; i * i < f.Length; ++i)
+            {
+                if (!f[i])
+                {
+                    for (int t = 2 * i; t < f.Length; t += i)
+                        f[i] = true;
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < f.Length; ++i)
+                if (!f[i])
+                    count++;
+
+            var res = new int[count];
+            var resCounter = 0;
+            for (int i = 0; i < f.Length; ++i)
+                if(!f[i])
+                {
+                    res[resCounter] = i;
+                    resCounter ++;
+                }
+
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if(PrimeGenerator.Generate(10).Length != 4)
+                return -1;
+
+            if(PrimeGenerator.Generate(100).Length != 25)
+                return -1;
+
+            return 0;
+        }
+    }
+}
